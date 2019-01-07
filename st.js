bf12500e93e97dcdf270424db36b5c6cbb148484
sen.mao <sen.mao@vipshop.com>
2019-01-07 15:59:00 +0800

    添加测试数据

--- st.js
+++ st.js
@@ -954,4 +954,31 @@
       transform: TRANSFORM.transform,
     };
   }
+
+  //测试
+  var template = {
+    "menu": {
+      "flavor": "{{flavor}}",
+      "richness": "{{richness}}",
+      "garlic amount": "{{garlic_amount}}",
+      "green onion?": "{{green_onion}}",
+      "sliced pork?": "{{pork_amount}}",
+      "secret sauce": "{{sauce_amount}}",
+      "noodle's texture": "{{texture}}"
+    }
+  }
+  
+  var data = {
+    "flavor": "strong",
+    "richness": "ultra rich",
+    "garlic_amount": "1 clove",
+    "green_onion": "thin green onion",
+    "pork_amount": "with",
+    "sauce_amount": "double",
+    "texture": "extra firm"
+  }
+  var result = TRANSFORM.transform(template, data)
+  console.log(result)
 }());
+
+
